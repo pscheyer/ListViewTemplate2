0a125291dea2f32a7e030a9531c4e60b860ea49e
Peter Scheyer <pscheyer@gmail.com>
2015-03-25 07:21:29 +0300

    20150325 0721qtime photo filters and sending to instagram

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Peter Scheyer on 3/25/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
